5d008b8751bea492a6bf1fc13b6ea19909212a99
Carles Figuerola <carlesgf@gmail.com>
2018-11-01 16:47:21 +0000

    Add rsvp modals, make coupons a variable

--- wedding/views.py
+++ wedding/views.py
@@ -7,4 +7,7 @@ def home(request):
     return render(request, 'home.html', context={
         'save_the_dates': SAVE_THE_DATE_CONTEXT_MAP,
         'support_email': settings.DEFAULT_WEDDING_REPLY_EMAIL,
+        'expedia_coupon': 'VOYAGEOCT',
+        'hcom_coupon': 'FNFQ118:AF5JU5',
+        'hcom_coupon_url': 'https://www.hotels.com/?rffrid=eml.hcom.UK.300.00.2018.10.30.src00.00.00.0000.0000.00.0000.DD00.kwrd=MER.AQ.TYU.eml.0.0.ff_booknow'
     })
