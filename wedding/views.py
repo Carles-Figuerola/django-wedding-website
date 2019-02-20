dde1e6c8490f3f634f9eeae51115e5d7a8538474
Carles Figuerola <carlesgf@gmail.com>
2019-02-20 01:27:16 +0000

    Add registry and wedding party pages

--- wedding/views.py
+++ wedding/views.py
@@ -4,10 +4,20 @@ from guests.save_the_date import SAVE_THE_DATE_CONTEXT_MAP
 
 
 def home(request):
+    query_params = request.GET
+    debug = False
+    if 'debug' in query_params:
+        if query_params['debug'].lower() == "true":
+            debug = True
+    mode = ""
+    if 'mode' in query_params:
+        mode = query_params['mode']
     return render(request, 'home.html', context={
         'save_the_dates': SAVE_THE_DATE_CONTEXT_MAP,
         'support_email': settings.DEFAULT_WEDDING_REPLY_EMAIL,
-        'expedia_coupon': 'VOYAGEOCT',
+        'expedia_coupon': 'BEYONDBORDERS',
         'hcom_coupon': 'FNFQ118:AF5JU5',
-        'hcom_coupon_url': 'https://www.hotels.com/?rffrid=eml.hcom.UK.300.00.2018.10.30.src00.00.00.0000.0000.00.0000.DD00.kwrd=MER.AQ.TYU.eml.0.0.ff_booknow'
+        'hcom_coupon_url': 'https://www.hotels.com/?rffrid=eml.hcom.UK.300.00.2018.10.30.src00.00.00.0000.0000.00.0000.DD00.kwrd=MER.AQ.TYU.eml.0.0.ff_booknow',
+        'debug': debug,
+        'mode': mode
     })
